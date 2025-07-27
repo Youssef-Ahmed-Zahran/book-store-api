efa0f280b521e49a669fdda79b85625e1ab9d00c
Youssef Ahmed <zahranyoussef57@gmail.com>
2025-07-27 14:12:27 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const logger = require("./middlewares/logger");
+const conectToDB = require("./config/db");
+const { notFound, errorHanlder } = require("./middlewares/errors");
+require("dotenv").config();
+
+// Connection To Database
+conectToDB();
+
+// Init App
+const app = express();
+
+//Apply Middlewares
+app.use(express.json());
+app.use(logger);
+
+// Routes
+app.use("/api/books", require("./routes/books"));
+app.use("/api/authors", require("./routes/authors"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/users", require("./routes/users"));
+
+// Error Hander Middleware
+app.use(notFound);
+app.use(errorHanlder);
+
+// Running the server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`));
